d339919a93ceb51f50cc37521dda16e73c8d6863
Vishnu Pandit <vish26385@gmail.com>
2026-04-15 12:48:44 +0530

    Fixed inputcontext and outputcontext length in LeadAiSuggestions

--- Modules/Leads/Configurations/LeadAiSuggestionConfiguration.cs
+++ Modules/Leads/Configurations/LeadAiSuggestionConfiguration.cs
@@ -6,19 +6,57 @@ namespace SaaSForge.Api.Modules.Leads.Configurations;
 
 public class LeadAiSuggestionConfiguration : IEntityTypeConfiguration<LeadAiSuggestion>
 {
+    //public void Configure(EntityTypeBuilder<LeadAiSuggestion> builder)
+    //{
+    //    builder.ToTable("LeadAiSuggestions");
+
+    //    builder.HasKey(x => x.Id);
+
+    //    builder.Property(x => x.SuggestionType).HasMaxLength(50).IsRequired();
+    //    builder.Property(x => x.InputContext).HasMaxLength(12000).IsRequired();
+    //    builder.Property(x => x.OutputText).HasMaxLength(12000).IsRequired();
+    //    builder.Property(x => x.Tone).HasMaxLength(50);
+    //    builder.Property(x => x.Goal).HasMaxLength(50);
+    //    builder.Property(x => x.Model).HasMaxLength(100);
+    //    builder.Property(x => x.CreatedAtUtc).HasColumnType("timestamp with time zone");
+
+    //    builder.HasOne(x => x.Lead)
+    //        .WithMany(x => x.AiSuggestions)
+    //        .HasForeignKey(x => x.LeadId)
+    //        .OnDelete(DeleteBehavior.Cascade);
+
+    //    builder.HasIndex(x => new { x.BusinessId, x.LeadId, x.CreatedAtUtc });
+    //}
+
     public void Configure(EntityTypeBuilder<LeadAiSuggestion> builder)
     {
         builder.ToTable("LeadAiSuggestions");
 
         builder.HasKey(x => x.Id);
 
-        builder.Property(x => x.SuggestionType).HasMaxLength(50).IsRequired();
-        builder.Property(x => x.InputContext).HasMaxLength(12000).IsRequired();
-        builder.Property(x => x.OutputText).HasMaxLength(12000).IsRequired();
-        builder.Property(x => x.Tone).HasMaxLength(50);
-        builder.Property(x => x.Goal).HasMaxLength(50);
-        builder.Property(x => x.Model).HasMaxLength(100);
-        builder.Property(x => x.CreatedAtUtc).HasColumnType("timestamp with time zone");
+        builder.Property(x => x.SuggestionType)
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.Property(x => x.InputContext)
+            .HasColumnType("text")
+            .IsRequired();
+
+        builder.Property(x => x.OutputText)
+            .HasColumnType("text")
+            .IsRequired();
+
+        builder.Property(x => x.Tone)
+            .HasMaxLength(50);
+
+        builder.Property(x => x.Goal)
+            .HasMaxLength(50);
+
+        builder.Property(x => x.Model)
+            .HasMaxLength(100);
+
+        builder.Property(x => x.CreatedAtUtc)
+            .HasColumnType("timestamp with time zone");
 
         builder.HasOne(x => x.Lead)
             .WithMany(x => x.AiSuggestions)
